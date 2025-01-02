b997291d2eb1ee7b1b6245299af00a63902a4057
John Meyer <johndmeyer@jmeyer.hsd1.co.comcast.net>
2025-01-02 12:42:56 -0700

    minor improvements

--- api/recipe-api-python-flask/data/mysql_utils.py
+++ api/recipe-api-python-flask/data/mysql_utils.py
@@ -1,30 +1,56 @@
+# https://dev.mysql.com/doc/connector-python/en/
 import mysql.connector
 
-dev call_sproc:
+def call_proc(proc_args, proc_name):
+	"""
+	Calls a stored procedure using the mysql.connector callproc method
+	:param proc_args: a list of name value pair dictionaries containing the proc arguments
+	:param proc_name: a string containing the name of the stored procedure
+	:return: returns a list of result sets, each set being a list of rows, each row being
+		a dictionary with the column name as the key and the value returned from mysql as 
+		the value - this can be mapped to the standard json api response format in the 
+		calling method (e.g. { data: { // json data here } }) 
+	"""
 
-# Establish a connection to the database
-mydb = mysql.connector.connect(
-  host="your_database_host",
-  user="your_username",
-  password="your_password",
-  database="your_database_name"
-)
+	# Establish a connection to the database
+	mysql_connection = mysql.connector.connect(
+	  host="localhost",
+	  user="root",
+	  password="abc123",
+	  database="recipe"
+	)
 
-# Create a cursor object
-mycursor = mydb.cursor()
+	# Create a cursor object
+	mysql_cursor = mysql_connection.cursor()
 
-# Define the parameters for the stored procedure
-procedure_name = "your_stored_procedure_name"
-param1 = "value1"
-param2 = 123
+	# Call the stored procedure
+	mysql_cursor.callproc(proc_name, [arg.get('arg_value') for arg in proc_args])
 
-# Call the stored procedure
-mycursor.callproc(procedure_name, (param1, param2))
+	# Build response object - associate column names with returned values - works with 
+	# multiple result sets
+	response = [];
 
-# Fetch the results (if any)
-for result in mycursor.stored_results():
-    print(result.fetchall())
+	# Iterate each result set in response
+	for stored_result in mysql_cursor.stored_results():
+		result_set = []
 
-# Close the cursor and connection
-mycursor.close()
-mydb.close()
+		#rows = stored_result.fetchall()
+		row_names = stored_result.column_names
+
+		# Iterate each row in the result set
+		for row in stored_result.fetchall():
+			result_row = {}
+
+			# Iterate each item in the row
+			for index, item in enumerate(row):
+				result_row[row_names[index]] = item
+
+			result_set.append(result_row)
+
+		response.append(result_set)
+		
+	# Close the cursor and connection
+	mysql_cursor.close()
+	mysql_connection.close()
+
+	return response
--- api/recipe-api-python-flask/logic/recipe.py
+++ api/recipe-api-python-flask/logic/recipe.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource
-from data.request import exec
+from data.mysql_utils import call_proc
 
 class Recipe(Resource):
 	def get(self, id):
@@ -8,4 +8,16 @@ class Recipe(Resource):
 			{'arg_name': 'recipeId', 'arg_value': id}
 		]
 
-		return exec(proc_name, proc_args)
+		result = call_proc(proc_args, proc_name)
+
+		data = {}
+
+		data['recipe'] = result[0][0]
+		data['ingredients'] = result[1]
+
+		response = {}
+
+		response['data'] = data
+
+		return response
+
